cc297b3444750524356900e5375ab86aee44bef8
H3lllfir3 <gholami.ayub73+github@gmail.com>
2023-11-03 20:34:09 +0100

    port changed.

--- lab02/app.py
+++ lab02/app.py
@@ -124,4 +124,4 @@ async def profile_template(request: Request, user: str = Cookie(None)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
